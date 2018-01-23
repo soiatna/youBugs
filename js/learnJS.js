a16f4527fd713e587691826e8cc52d2ff5528b85
soiatna <soiatna@inbox.ru>
2018-01-23 23:18:17 +0300

    Массив для отрисовки шахматной доски 0.0.2.0

--- js/learnJS.js
+++ js/learnJS.js
@@ -1,9 +1,9 @@
-var tableBody = '';
-var tdCellsBlack = '';
-var tdCellsWhite = '';
-var i;
-var j;
-var rows;
+var tableBody = '',
+    tdCellsBlack = '',
+    tdCellsWhite = '',
+    i,
+    j,
+    rows;
 
 for (j = 1; j < 9; j++) {
     if((j%2) == 0) {
@@ -37,5 +37,27 @@ for (i = 1; i < 9; i++) {
 $('body').append ('<table></table>');
 $('table').append(tableBody);
 
+var colorCell = ["black", "white"],
+    cheesLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+    cheesNumber = ['1', '2', '3', '4', '5', '6', '7', '8'],
+    cheesFigure = [null, 'pawn', 'knight', 'bishop', 'queen', 'king'],
+    cheesCell = [],
+    numberColorCell;
 
 
+
+for (var x = 0; x < 8; x++) {
+        cheesCell[x]=[];
+
+        for (var y = 0; y < 8; y++) {
+            if(x % 2 == 0 && y % 2 !== 0) {
+                numberColorCell = colorCell[0];
+            } else if (x % 2 !== 0 && y % 2 == 0) {
+                numberColorCell = colorCell[0];
+            } else {
+                numberColorCell = colorCell[1];
+            }
+            cheesCell[x][y] = [cheesLetter[x], cheesNumber[y], numberColorCell, cheesFigure [0]];
+            }
+        }
+console.log(cheesCell);
